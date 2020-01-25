bfb546bd8fc764c5b0be67253c53510e723c011b
RH <rh>
2020-01-25 18:30:31 +0100

    lala

--- main.c
+++ main.c
@@ -11,17 +11,20 @@ int  main(int ac, char **av)
 {
 	t_fdf	*data;
 	int ret = 0; //
-	if (ac == 2)
-	{
+	if (ac != 2)
+			printf("Missing argument\n");
 	data = (t_fdf *)malloc(sizeof(t_fdf));
-	read_file(data, av[1]);
+	if(!(read_file(data, av[1])))
+		printf("File error\n");
 	data->mlx_ptr = mlx_init();
 	data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "FDF");
-	draw_matrice(data);
-	mlx_key_hook(data->win_ptr, deal_key, NULL);
+	//data->img->img_ptr = mlx_new_image(data->mlx_ptr,500, 500);
+	//data->img->data = (int *)mlx_get_data_addr(data->img->img_ptr, &data->img->bpp, &data->img->size, &data->img->endian);
+
+//	draw_matrice(data);
+
+	//mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->img_ptr, 200, 200);
+	//mlx_key_hook(data->win_ptr, deal_key, NULL);
 	mlx_loop(data->mlx_ptr);
-	}
-	else
-		printf("Missing argument\n");
 	return 0;
 }
--- read.c
+++ read.c
@@ -1,23 +1,26 @@
 #include "fdf.h"
 
-void		get_height_width(t_fdf *data, char *file_name)
+int		get_height_width(t_fdf *data, char *file_name)
 {
 	char *line;
-	int width_check;
+	//int width_check;
 	int fd;
 
+	//width_check = 0;
 	fd = open(file_name, O_RDONLY);
 	data->height = 0;
 	data->width = 0;
 	while((get_next_line(fd, &line) != 0))
 	{
-		width_check = ft_countwords(line, ' ');
-		if(width_check > data->width)
-			data->width = width_check;
+		data->width = ft_countwords(line, ' ');
+		//if(width_check != data->width) //
+		//	return (0);
 		data->height++;
 		free(line);
+		//width_check = data->width;
 	}
 	close(fd);
+	return(1);
 }
 
 int	fill_matrix(int *z_line, char *line)
@@ -50,7 +53,8 @@ int	read_file(t_fdf	*data, char *file_name)
 	char *line;
 	int i;
 
-	get_height_width(data, file_name);
+	if(!(get_height_width(data, file_name)))
+		return(0);
 	if(!(data->matrix_z = (int **)malloc(sizeof(int *) * (data->height + 1))))
 			return (0); //free something ?
 	i = 0;
